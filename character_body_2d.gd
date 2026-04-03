b50480509c1c29236c813b239b75c66ddf109c55
DanielMelnikoff <mcheaf1111@gmail.com>
2026-04-03 11:40:45 +0300

    edit-controls

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -16,10 +16,12 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	if Input.is_key_pressed(KEY_D):
+		velocity.x = SPEED
+	elif Input.is_key_pressed(KEY_A):
+		velocity.x = -SPEED
+	else: velocity.x = 0
+		#move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
