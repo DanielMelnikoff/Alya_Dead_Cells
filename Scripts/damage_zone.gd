7d9760512249d25d24e81dab6c061fc70cac54a5
DanielMelnikoff <mcheaf1111@gmail.com>
2026-04-04 14:53:45 +0300

    refactored-arcitecture

--- Scripts/damage_zone.gd
+++ Scripts/damage_zone.gd
@@ -8,25 +8,19 @@ func _ready():
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 	$Timer.timeout.connect(_on_timer_timeout)
-	
-	# Настройки таймера (на всякий случай)
-	$Timer.wait_time = 1.0
-	$Timer.one_shot = false
 
 func _on_body_entered(body):
-	# Проверяем, что вошел именно игрок
 	if body.is_in_group("player"):
 		player_inside = body
-		do_action()      # Делаем действие ПЕРВЫЙ РАЗ сразу при входе
-		$Timer.start()   # Запускаем повторы каждую секунду
+		do_action()      
+		$Timer.start()  
 
 func _on_body_exited(body):
 	if body == player_inside:
 		player_inside = null
-		$Timer.stop()    # Игрок вышел — выключаем повторы
+		$Timer.stop() 
 
 func _on_timer_timeout():
-	# Это срабатывает каждую секунду, пока игрок внутри
 	if player_inside:
 		do_action()
 func do_action():
