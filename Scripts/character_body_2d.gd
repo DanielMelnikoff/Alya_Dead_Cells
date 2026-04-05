d8867e5bd4fafda05c2b2941392e22a98416c36d
DanielMelnikoff <mcheaf1111@gmail.com>
2026-04-05 10:20:57 +0300

    add-double-jump

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -2,8 +2,9 @@ extends CharacterBody2D
 
 
 const SPEED = 500.0
-const JUMP_VELOCITY = -600.0
+const JUMP_VELOCITY = -700.0
 var HP = 100
+var jumpable = true
 
 func take_damage(amount):
 	HP -= amount
@@ -14,15 +15,17 @@ func take_damage(amount):
 func die():
 	get_tree().reload_current_scene()
 
-var rand = RandomNumberGenerator.new()
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+	if is_on_floor():
+		jumpable=true
+	if not is_on_floor() and Input.is_action_just_pressed("ui_accept") and jumpable:
+		velocity.y = JUMP_VELOCITY
+		jumpable = false
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
